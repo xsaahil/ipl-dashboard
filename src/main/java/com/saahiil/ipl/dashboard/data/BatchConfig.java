4de00e4e26dd7dd9bf7fb4d0ca003f2f13bca567
Sahil Shaikh <sahil.shaikh@republic.co>
2023-07-17 23:41:08 +0530

    This commit pulls data from the csv file and successfully processes it and also gives out the team names and the match time!

---
+++ src/main/java/com/saahiil/ipl/dashboard/data/BatchConfig.java
@@ -0,0 +1,82 @@
+package com.saahiil.ipl.dashboard.data;
+
+import com.saahiil.ipl.dashboard.model.Match;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BatchConfig {
+
+    private final String[] FIELD_NAMES = new String[] {
+            "ID","City","Date","Season","MatchNumber","Team1","Team2","Venue","TossWinner","TossDecision","SuperOver","WinningTeam","WonBy","Margin","method","Player_of_Match","Team1Players","Team2Players","Umpire1","Umpire2"
+    };
+
+    @Bean
+    public FlatFileItemReader<MatchInput> reader() {
+        return new FlatFileItemReaderBuilder<MatchInput>().name("MatchItemReader")
+                .resource(new ClassPathResource("match-data.csv")).delimited().names(FIELD_NAMES)
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<MatchInput>() {
+                    {
+                        setTargetType(MatchInput.class);
+                    }
+                }).build();
+    }
+
+    @Bean
+    public MatchDataProcessor processor() {
+        return new MatchDataProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Match> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Match>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("INSERT INTO match (id, city, date, team1, team2, venue, toss_winner," +
+                        "toss_decision, match_winner) VALUES (:id, :city, :date, :team1, " +
+                        ":team2, :venue, :tossWinner, :tossDecision, :matchWinner)")
+                .dataSource(dataSource)
+                .build();
+    }
+
+    @Bean
+    public Job importUserJob(JobRepository jobRepository,
+                             JobCompletionNotificationListener listener, Step step1) {
+        return new JobBuilder("importUserJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .listener(listener)
+                .flow(step1)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository,
+                      PlatformTransactionManager transactionManager, JdbcBatchItemWriter<Match> writer) {
+        return new StepBuilder("step1", jobRepository)
+                .<MatchInput, Match> chunk(10, transactionManager)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer)
+                .build();
+    }
+
+}
+
+
---
+++ src/main/java/com/saahiil/ipl/dashboard/data/MatchInput.java
@@ -0,0 +1,186 @@
+package com.saahiil.ipl.dashboard.data;
+
+public class MatchInput {
+    private String ID;
+    private String City;
+    private String Date;
+    private String Season;
+    private String MatchNumber;
+    private String Team1;
+    private String Team2;
+    private String Venue;
+    private String TossWinner;
+    private String TossDecision;
+    private String SuperOver;
+    private String WinningTeam;
+    private String WonBy;
+    private String Margin;
+    private String method;
+    private String Player_of_Match;
+    private String Team1Players;
+    private String Team2Players;
+    private String Umpire1;
+    private String Umpire2;
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public String getSeason() {
+        return Season;
+    }
+
+    public String getMatchNumber() {
+        return MatchNumber;
+    }
+
+    public String getTeam1() {
+        return Team1;
+    }
+
+    public String getTeam2() {
+        return Team2;
+    }
+
+    public String getVenue() {
+        return Venue;
+    }
+
+    public String getTossWinner() {
+        return TossWinner;
+    }
+
+    public String getTossDecision() {
+        return TossDecision;
+    }
+
+    public String getSuperOver() {
+        return SuperOver;
+    }
+
+    public String getWinningTeam() {
+        return WinningTeam;
+    }
+
+    public String getWonBy() {
+        return WonBy;
+    }
+
+    public String getMargin() {
+        return Margin;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPlayer_of_Match() {
+        return Player_of_Match;
+    }
+
+    public String getTeam1Players() {
+        return Team1Players;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public void setSeason(String season) {
+        Season = season;
+    }
+
+    public void setMatchNumber(String matchNumber) {
+        MatchNumber = matchNumber;
+    }
+
+    public void setTeam1(String team1) {
+        Team1 = team1;
+    }
+
+    public void setTeam2(String team2) {
+        Team2 = team2;
+    }
+
+    public void setVenue(String venue) {
+        Venue = venue;
+    }
+
+    public void setTossWinner(String tossWinner) {
+        TossWinner = tossWinner;
+    }
+
+    public void setTossDecision(String tossDecision) {
+        TossDecision = tossDecision;
+    }
+
+    public void setSuperOver(String superOver) {
+        SuperOver = superOver;
+    }
+
+    public void setWinningTeam(String winningTeam) {
+        WinningTeam = winningTeam;
+    }
+
+    public void setWonBy(String wonBy) {
+        WonBy = wonBy;
+    }
+
+    public void setMargin(String margin) {
+        Margin = margin;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public void setPlayer_of_Match(String player_of_Match) {
+        Player_of_Match = player_of_Match;
+    }
+
+    public void setTeam1Players(String team1Players) {
+        Team1Players = team1Players;
+    }
+
+    public void setTeam2Players(String team2Players) {
+        Team2Players = team2Players;
+    }
+
+    public void setUmpire1(String umpire1) {
+        Umpire1 = umpire1;
+    }
+
+    public void setUmpire2(String umpire2) {
+        Umpire2 = umpire2;
+    }
+
+    public String getTeam2Players() {
+        return Team2Players;
+    }
+
+    public String getUmpire1() {
+        return Umpire1;
+    }
+
+    public String getUmpire2() {
+        return Umpire2;
+    }
+
+
+}
